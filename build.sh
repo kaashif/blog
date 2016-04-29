865d9fa2ed0117f8c1f95b177f693d9a1b94e3c7
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2016-04-29 19:04:27 +0100

    modded build script to build archive

--- build.sh
+++ build.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
 rm -rf _out/
 
+#set archive to the index
+cp _src/archive.html _src/index-template.html
+
+#build the site
 raco frog -b
+
+#move index page to archive
+mv _out/index.html _out/archive.html
+
+#set index to the index
+rm _src/index-template.html
+cp _src/index.html _src/index-template.html
+
+#build the site again to get the normal index
+raco frog -b
+
+#delete the copied index
+rm _out/index-template.html
+
+#copy the other dirs
 cp -r css _out/
 cp -r static _out/
+
+#preview the site
 raco frog -p
 

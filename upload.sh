1c047a19adb079047a90f3ff6321d37fa79a2493
kaashif <kaashif@kaashif.co.uk>
2016-05-03 17:27:53 +0000

    added aws upload script

--- upload.sh
+++ upload.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 cd _out
-git init
-git --git-dir=./.git add .
-git --git-dir=./.git commit -m 'rebuilt'
-git --git-dir=./.git push -f ssh://55193ee3fcf9334054000012@blog-kaashif.rhcloud.com/~/git/blog.git/ master
 
+#old openshift thing
+#git init
+#git --git-dir=./.git add .
+#git --git-dir=./.git commit -m 'rebuilt'
+#git --git-dir=./.git push -f ssh://55193ee3fcf9334054000012@blog-kaashif.rhcloud.com/~/git/blog.git/ master
+
+#new ec2 thing
+tar czf - . | ssh -i "~/misc/blog-aws.pem" ubuntu@ec2-52-16-74-211.eu-west-1.compute.amazonaws.com 'cat - > ~/site.tgz; sudo rm -rf /usr/share/nginx/html/*; sudo tar -C /usr/share/nginx/html -xvzf site.tgz'

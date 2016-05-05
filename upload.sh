a9ffcd3ad801bd5a5fae9fa4f04f2254bf7d445f
kaashif <kaashif@kaashif.co.uk>
2016-05-05 01:06:54 +0000

    fixed

--- upload.sh
+++ upload.sh
@@ -9,4 +9,4 @@ cd _out
 #git --git-dir=./.git push -f ssh://55193ee3fcf9334054000012@blog-kaashif.rhcloud.com/~/git/blog.git/ master
 
 #new ec2 thing
-tar czf - . | ssh -i "~/misc/blog-aws.pem" ubuntu@ec2-52-16-74-211.eu-west-1.compute.amazonaws.com 'cat - > ~/site.tgz; sudo rm -rf /usr/share/nginx/html/*; sudo tar -C /usr/share/nginx/html -xvzf site.tgz'
+tar czf - . | ssh -i "~/misc/important/blog-aws.pem" ubuntu@ec2-52-16-74-211.eu-west-1.compute.amazonaws.com 'cat - > ~/site.tgz; sudo rm -rf /usr/share/nginx/html/*; sudo tar -C /usr/share/nginx/html -xvzf site.tgz'

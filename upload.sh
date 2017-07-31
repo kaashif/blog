28938319a86ec762797b83f9ed5fa9e7998ecb8c
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2017-07-31 15:34:15 +0100

    remove old tarball

--- upload.sh
+++ upload.sh
@@ -3,4 +3,4 @@
 cd _out
 
 #new digitalocean thing
-tar czf - . | ssh earendil.kaashif.co.uk 'cat - > ~/site.tgz; doas rm -rf /var/www/htdocs/*; doas tar -C /var/www/htdocs -xvzf site.tgz'
+tar czf - . | ssh earendil.kaashif.co.uk 'rm ~/site.tgz; cat - > ~/site.tgz; doas rm -rf /var/www/htdocs/*; doas tar -C /var/www/htdocs -xvzf ~/site.tgz'

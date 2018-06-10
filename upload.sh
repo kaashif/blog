ce996b87c65c87072d770005a9d44c53aebf5d21
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2018-06-10 21:03:52 +0100

    use rsync to upload site

--- upload.sh
+++ upload.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-cd _out
-
-#new digitalocean thing
-tar czf - . | ssh earendil.kaashif.co.uk 'rm ~/site.tgz; cat - > ~/site.tgz; doas rm -rf /var/www/htdocs/*; doas tar -C /var/www/htdocs -xvzf ~/site.tgz'
+rsync -avuz site/ mail.kaashif.co.uk:/var/www/htdocs/

3c78718c8e4f7ed6fcfc6277fee3cf6fed7d518b
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2018-07-07 11:54:23 +0100

    some kind of script

---
+++ posts/fix.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use HTML::Escape qw(escape_html);
+
+my $in_code_block = 0;
+while (<>) {
+  if (/```/) {
+    if ($in_code_block) {
+      print "</code></pre>\n";
+    } else {
+      print "<pre><code>\n";
+    }
+    $in_code_block = !$in_code_block;
+  } else {
+    if ($in_code_block) {
+      print(escape_html($_));
+    } else {
+      print($_);
+    }
+  }
+}
